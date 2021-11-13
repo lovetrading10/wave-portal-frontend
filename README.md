4046e41326cc0a1192d10e0433856e0b2019b0ee
lovetrading10 <lovetrading09@yahoo.com>
2021-11-13 13:35:34 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Wave Portal Frontend
+
+> Disclaimer: project implemented in a half-day hackathon, I will soon revisit it to improve so many things
+
+Send me a hello through the blockchain!
+
+This is my project for the course [Build a Web3 App with Solidity + Ethereum Smart Contracts](https://buildspace.so/solidity) at [Buildspace](https://buildspace.so/).
+
+Check the backend code at https://github.com/fmoliveira/wave-portal-backend
+
+## Prerequisites
+
+- Node.js v14+
+- MetaMask browser extension
+
+## Getting started
+
+Follow the steps below to run the project locally:
+
+- Install dependencies: `yarn install`
+- Run the application locally: `yarn start`
+- Access the application at http://localhost:3000/
---
+++ src/components/Wallet.js
@@ -0,0 +1,56 @@
+import React from "react";
+import classNames from "classnames";
+
+export default function Wallet({
+	loading,
+	walletInstalled,
+	walletConnected,
+	networkName,
+	isRinkeby,
+	connectWallet,
+}) {
+	if (loading) {
+		return <div className="buttonGroup" />;
+	}
+
+	return (
+		<div className="buttonGroup justifyCenter fading">
+			{!walletInstalled && (
+				<a
+					className="button buttonNoWallet"
+					href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn/related"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Install MetaMask
+				</a>
+			)}
+			{walletInstalled && !walletConnected && (
+				<button className="button buttonMetaMask" onClick={connectWallet}>
+					Connect MetaMask
+				</button>
+			)}
+			{walletConnected && (
+				<div>
+					<div>
+						<span className="dotConnected" />
+						Wallet Connected
+					</div>
+					<div
+						className={classNames(
+							"network",
+							isRinkeby ? "networkValid" : "networkInvalid",
+						)}
+					>
+						Network: <span className="networkName">{networkName}</span>
+					</div>
+					{!isRinkeby && (
+						<div className="network networkInvalid">
+							Please switch to Rinkeby
+						</div>
+					)}
+				</div>
+			)}
+		</div>
+	);
+}
